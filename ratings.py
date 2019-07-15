ed889e953f32c04cdc0c2a4413e7e3f02066d3db
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-15 16:32:26 -0700

    changed get restaurant ratings function to add ratings | added completed get user option function

--- ratings.py
+++ ratings.py
@@ -1,5 +1,11 @@
 # from pprint import pprint
 
+INSTRUCTIONS = """ 
+[S]eeing all restaurant ratings
+[A]dding new restaurant ratings
+[Q]uit app
+"""
+
 filename = open("scores.txt")
 
 
@@ -11,16 +17,7 @@ def print_restaurants(rest_dict):
         print(f' {restr} is rated at {rating}.')
 
 
-
-def get_restaurant_ratings(file):
-
-    restaurants = {}
-
-    for line in file:
-        restaurant_data = line.rstrip().split(":")
-        restaurants[restaurant_data[0]] = restaurant_data[1]
-
-    print_restaurants(restaurants)
+def add_ratings(rest_dict):
 
     rest_name = input("Give me the restaurant name: ")
     rest_rating = input("Give me the restaurant rating: ")
@@ -34,10 +31,33 @@ def get_restaurant_ratings(file):
             break
  
 
-    restaurants[rest_name] = rest_rating
-
-    print_restaurants(restaurants)
+    rest_dict[rest_name] = rest_rating
+    return rest_dict
 
     # pprint(rest_sorted)
 
-get_restaurant_ratings(filename)
+
+def get_user_options(file):
+
+    restaurants = {}
+
+    for line in file:
+        restaurant_data = line.rstrip().split(":")
+        restaurants[restaurant_data[0]] = restaurant_data[1]
+
+    while True:
+        print(INSTRUCTIONS)
+        user_choice = input("What would you like to do? ")
+        user_choice = user_choice.upper()
+        
+        if user_choice == "S":
+            print_restaurants(restaurants)
+        elif user_choice == "A":
+            restaurants = add_ratings(restaurants)
+        elif user_choice == "Q":
+            break
+        else:
+            print("invalid response: do better!!!!")
+
+
+get_user_options(filename)

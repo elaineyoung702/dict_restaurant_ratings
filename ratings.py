a6613350d641d7d178200de25c900f380cf5ed48
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-15 13:04:59 -0700

    added user input for new restaurants

--- ratings.py
+++ ratings.py
@@ -10,9 +10,20 @@ def get_restaurant_ratings(file):
     for line in file:
         restaurant_data = line.rstrip().split(":")
         restaurants[restaurant_data[0]] = restaurant_data[1]
-    
+
     rest_sorted = (sorted(restaurants.items()))
 
+    for restr, rating in rest_sorted:
+        print(f' {restr} is rated at {rating}.')
+
+    rest_name = input("Give me the restaurant name: ")
+    rest_rating = input("Give me the restaurant rating: ")
+    
+
+    restaurants[rest_name] = rest_rating
+
+    rest_sorted = (sorted(restaurants.items()))
+    
     for restr, rating in rest_sorted:
         print(f' {restr} is rated at {rating}.')
 
